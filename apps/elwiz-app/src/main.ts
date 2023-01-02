46cc04a540c0048300e3f58b14b0a1c096dc9e9a
Ronnie Henriksen Laugen <rhenri@equinor.com>
2023-01-02 08:09:06 +0100

    Inject httpClient

--- apps/elwiz-app/src/main.ts
+++ apps/elwiz-app/src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserAnimationsModule)
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient()
   ],
 }).catch((err) => console.error(err));
